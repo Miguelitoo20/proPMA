2ead8fa634878e1bbfd03f2ef190df7ae9365366
MiguelFloresVillarroel420 <miguel.flores.v@tecsup.edu.pe>
2024-05-22 09:11:30 -0500

    Initial Commit

---
+++ proPMA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proPMA
+//
+//  Created by Miguel Flores V on 22/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
